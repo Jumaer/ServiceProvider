deb26567be97841187d4b84fa127bd438c04d748
jumaer-ahamed <jumaercse@gmail.com>
2024-04-25 20:46:23 +0600

    added update for hilt and api service

---
+++ app/src/main/java/com/friendship/bhaibhaiclinic/networking/ApiEndpoint.kt
@@ -0,0 +1,15 @@
+package com.friendship.bhaibhaiclinic.networking
+
+object ApiEndpoint {
+
+    /**
+     * API services helper prefix urls..
+     * api endpoint list for data (I/O) operations..
+     */
+
+    const val ENDPOINT_GET_PROVIDER_LIST = "public/v2/users"
+    const val ENDPOINT_POST_PROVIDER_CREATE= "public/v2/users"
+    const val ENDPOINT_PUT_PROVIDER = "public/v2/users/{user-id}"
+
+
+}
---
+++ app/src/main/java/com/friendship/bhaibhaiclinic/networking/ApiService.kt
@@ -0,0 +1,38 @@
+package com.friendship.bhaibhaiclinic.networking
+
+import com.friendship.bhaibhaiclinic.networking.ApiEndpoint.ENDPOINT_GET_PROVIDER_LIST
+import com.friendship.bhaibhaiclinic.networking.ApiEndpoint.ENDPOINT_POST_PROVIDER_CREATE
+import com.friendship.bhaibhaiclinic.networking.ApiEndpoint.ENDPOINT_PUT_PROVIDER
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface ApiService {
+
+    @GET(ENDPOINT_GET_PROVIDER_LIST)
+    suspend fun getProviderList(
+        @Header("Authorization") typeToken: String,
+    ): Response<ResponseBody>
+
+
+    @PUT(ENDPOINT_PUT_PROVIDER)
+    suspend fun updateProvider(
+        @Header("Authorization") typeToken: String,
+        @Path("user-id") userId: String,
+        @Body body: RequestBody
+    ): Response<ResponseBody>
+
+
+    @POST(ENDPOINT_POST_PROVIDER_CREATE)
+    suspend fun createProvider(
+        @Header("Authorization") typeToken: String,
+        @Path("id") id: String,
+        @Body body: RequestBody
+    ): Response<ResponseBody>
+}
