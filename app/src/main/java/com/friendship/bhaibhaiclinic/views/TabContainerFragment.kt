d433b1bf6b371dc8b5bbc90d1ce1dc298f9314a1
jumaer-ahamed <jumaercse@gmail.com>
2024-04-26 12:33:39 +0600

    test api call is sucessfull

--- app/src/main/java/com/friendship/bhaibhaiclinic/views/TabContainerFragment.kt
+++ app/src/main/java/com/friendship/bhaibhaiclinic/views/TabContainerFragment.kt
@@ -1,21 +1,27 @@
 package com.friendship.bhaibhaiclinic.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.friendship.bhaibhaiclinic.R
-import com.friendship.bhaibhaiclinic.databinding.FragmentChangeProviderBinding
+import androidx.fragment.app.viewModels
+
 import com.friendship.bhaibhaiclinic.databinding.FragmentTabContainerBinding
+import com.friendship.bhaibhaiclinic.model.ProviderListResponse
+import com.friendship.bhaibhaiclinic.networking.DataState
+import com.friendship.bhaibhaiclinic.view_model.ProviderViewModel
+import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class TabContainerFragment : Fragment() {
 
 
     private lateinit var binding: FragmentTabContainerBinding
-
+    private val viewModel:ProviderViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,10 +30,44 @@ class TabContainerFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentTabContainerBinding.inflate(inflater, container, false)
 
+        observeProviders()
 
+        context?.let { viewModel.getProviders(it) }
 
         return binding.root
     }
 
+    private fun observeProviders() {
+        viewModel.getProviders.observe(viewLifecycleOwner) {
+            when (it) {
+                is DataState.Success -> {
+                    // get data
+                    val body = it.value.body()?.string()
+                    if (body.isNullOrBlank()) {
+
+                    } else {
+
+                        val response =
+                            Gson().fromJson(body, ProviderListResponse::class.java)
+                        Log.e("_TEST_TAG", response.toString())
+
+                    }
+                   // loadingDialog.dismiss()
+                }
+
+                is DataState.Loading -> {
+                  //  loadingDialog.show()
+                    Log.e("_TEST_TAG", "loadingDialog")
+                }
+
+                is DataState.Error -> {
+
+                   // loadingDialog.dismiss()
+                    Log.e("_TEST_TAG", "loadingDismiss")
+                }
+
+            }
+        }
+    }
 
 }
